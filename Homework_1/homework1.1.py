17ba91bb9457d7f068b430720b0c4004403dd699
Ekaterina_Vaganova <kat-vaganova@yandex.ru>
2021-09-05 12:05:13 +0600

    Homework_1

---
+++ Homework_1/homework1.1.py
@@ -0,0 +1,14 @@
+# 1. Поработайте с переменными, создайте несколько, выведите на экран,
+# запросите у пользователя несколько чисел и строк и сохраните в переменные,
+# выведите на экран
+
+first_word = 'Казнить'
+second_word = "нельзя"
+comma = ','
+third_word = "помиловать"
+print(first_word)
+print(second_word)
+print(third_word)
+print(first_word, second_word, comma, third_word, '!')
+print(f"{first_word} {second_word}, {third_word} !")
+print("{1} {0}, {2}!".format(first_word, second_word, third_word))
---
+++ Homework_1/homework1.4.py
@@ -0,0 +1,12 @@
+# 4. Пользователь вводит целое положительное число.
+# Найдите самую большую цифру в числе.
+# Для решения используйте цикл while и арифметические операции.
+
+value = int(input("Введите число: "))
+max_number = value % 10
+value = value // 10
+while value > 0:
+    if value % 10 > max_number:
+        max_number = value % 10
+    value = value // 10
+print("Самая большая цифра в числе: ", max_number)
